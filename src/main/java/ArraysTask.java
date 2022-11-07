2f1a314e9095a370c9ddb0b9c6ee1ae7b39d2377
Viktor Oblapenko <viktor.oblapenko@gmail.com>
2022-11-08 01:31:57 +0200

    New commit 19.50

--- src/main/java/ArraysTask.java
+++ src/main/java/ArraysTask.java
@@ -1,69 +1,137 @@
+
+
 import java.util.Arrays;
 
 public class ArraysTask {
+    static final int[] array1 = new int[]{2, 4, -6, 12, -65, 66, 34, 86, -3, 54, 56, 44, -32, 23, 45, 98, 43, -42, 22, 11, -189};
+
     public static void main(String[] args) {
-
-        int[] array1 = new int[]{2, 4, -6, 12, -65, 66, 34, 86, -3, 54, 56, 44, -32, 23, 45, 98, 43, -42, 22, 11, -189};
-        int amount = 0;
-        int sum = 0;
-        int minNegativeNumber = -1;
-        int minPositiveNumber = 2147483647;
-        int maxPositiveNumber = 0;
-        int maxNegativeNumber = -1;
-        int[] copyArray1 = Arrays.copyOf(array1, array1.length);
-        int[] copyArray2 = Arrays.copyOf(array1, array1.length);
-        int[] copyArray3 = Arrays.copyOf(array1, array1.length);
-
-        for (int i = 0; i < array1.length; i++) {
-            if (array1[i] % 2 == 0) {
-                sum += array1[i];
-                amount++;
-            }
-            if (array1[i] < 0 && array1[i] < minNegativeNumber) {
-                minNegativeNumber = array1[i];
-            }
-
-            if (array1[i] > 0 && array1[i] < minPositiveNumber) {
-                minPositiveNumber = array1[i];
-            }
-            {
-                if (array1[i] > 0) {
-                    copyArray1[i] += 1;
-                }
-                if (array1[i] < 0) {
-                    copyArray1[i] -= 1;
-                }
-            }
-            {
-                if (array1[i] > maxPositiveNumber) {
-                    maxPositiveNumber = array1[i];
-                }
-                if (array1[i] < maxNegativeNumber) {
-                    maxNegativeNumber = array1[i];
-                }
-            }
-
-        }
-        for (int j = 0; j < array1.length; j++) {
-            if(array1[j] == maxPositiveNumber) {
-                copyArray2[j] = maxNegativeNumber;
-            }
-            if(array1[j] == maxNegativeNumber) {
-                copyArray2[j] = maxPositiveNumber;
-            }
-        }
-
-        System.out.println("Amount of even elements : " + amount);
-        System.out.println("Sum of pair elements : " + sum);
-        System.out.println("Minimal negative number is " + minNegativeNumber);
-        System.out.println("Minimal positive number is " + minPositiveNumber);
+        printSumAndAmountEvenElements();
+        printMinNegativeAndMinPositiveElements();
         System.out.println("The original array: \n" + Arrays.toString(array1));
-        System.out.println("A new array with positive elements increased by one and negative elements decreased by one : \n" + Arrays.toString(copyArray1));
-        System.out.println("Maximum negative number is " + maxPositiveNumber);
-        System.out.println("Maximum positive number is " + maxNegativeNumber);
-        System.out.println("A new array with changed maximum and minimum : \n" + Arrays.toString(copyArray2));
-
+        System.out.println("A new array with positive elements increased by one " +
+                "and negative elements decreased by one : \n" + Arrays.toString(increasePositiveAndDecreaseNegativeByOne()));
+        System.out.println("Maximum positive number is " + findMaxValue());
+        System.out.println("Maximum negative number is " + findMinValue());
+        System.out.println("A new array with changed maximum and minimum : \n" + Arrays.toString(changeMaxAndMin()));
+        System.out.println("The array with deleted max and min values : \n" + Arrays.toString(deleteMaxAndMin()));
+        System.out.println("The arithmetic mean of the array is " + calculateArithmeticAverage());
+        System.out.println("The sum of the elements that are greater than the arithmetic mean of the array is " + findAmountOfElementsMoreThanAverage());
 
     }
 
+    public static void printSumAndAmountEvenElements() {
+        int amount = 0;
+        int sum = 0;
+        for (int j : array1) {
+            if (j % 2 == 0) {
+                sum += j;
+                amount++;
+            }
+        }
+        System.out.println("Amount of even elements : " + amount);
+        System.out.println("Sum of pair elements : " + sum);
+    }
+
+    public static void printMinNegativeAndMinPositiveElements() {
+        int edgeMinNegativeNumber = -2147483648;
+        int edgeMinPositiveNumber = 2147483647;
+        for (int j : array1) {
+            if (j < 0 && j > edgeMinNegativeNumber) {
+                edgeMinNegativeNumber = j;
+            }
+            if (j > 0 && j < edgeMinPositiveNumber) {
+                edgeMinPositiveNumber = j;
+            }
+        }
+        System.out.println("Minimal negative number is " + edgeMinNegativeNumber);
+        System.out.println("Minimal positive number is " + edgeMinPositiveNumber);
+    }
+
+    public static int[] increasePositiveAndDecreaseNegativeByOne() {
+        int[] copyArray = Arrays.copyOf(array1, array1.length);
+        for (int i = 0; i < array1.length; i++) {
+            if (array1[i] > 0) {
+                copyArray[i] += 1;
+            }
+            if (array1[i] < 0) {
+                copyArray[i] -= 1;
+            }
+        }
+        return copyArray;
+    }
+
+    public static int findMaxValue() {
+        int tempForMaximum = 0;
+        for (int j : array1) {
+            if (j > tempForMaximum) {
+                tempForMaximum = j;
+            }
+        }
+        return tempForMaximum;
+    }
+
+    public static int findMinValue() {
+        int tempForMinimum = -1;
+        for (int j : array1) {
+            if (j < tempForMinimum) {
+                tempForMinimum = j;
+            }
+        }
+        return tempForMinimum;
+    }
+
+    public static int[] changeMaxAndMin() {
+        int[] copyArray = Arrays.copyOf(array1, array1.length);
+        for (int j = 0; j < array1.length; j++) {
+            if (array1[j] == findMaxValue()) {
+                copyArray[j] = findMinValue();
+            }
+            if (array1[j] == findMinValue()) {
+                copyArray[j] = findMaxValue();
+            }
+        }
+        return copyArray;
+    }
+
+    public static int[] deleteMaxAndMin() {
+        int[] copyArray1 = new int[array1.length - 1];
+        int[] copyArray2 = new int[copyArray1.length - 1];
+        int tempMaxIndex = 0;
+        int tempMinIndex = 0;
+        for (int j = 0; j < array1.length; j++) {
+            if (array1[j] == findMaxValue()) {
+                tempMaxIndex = j;
+            }
+        }
+        System.arraycopy(array1, 0, copyArray1, 0, tempMaxIndex);
+        System.arraycopy(array1, tempMaxIndex + 1, copyArray1, tempMaxIndex, array1.length - tempMaxIndex - 1);
+        for (int i = 0; i < copyArray1.length; i++) {
+            if (copyArray1[i] == findMinValue()) {
+                tempMinIndex = i;
+            }
+        }
+        System.arraycopy(copyArray1, 0, copyArray2, 0, tempMinIndex);
+        System.arraycopy(copyArray1, tempMinIndex, copyArray2, tempMinIndex, copyArray1.length - tempMinIndex - 1);
+
+        return copyArray2;
+    }
+
+    public static int calculateArithmeticAverage() {
+        int sum = 0;
+        for (int j : array1) {
+            sum += j;
+        }
+        return sum / array1.length;
+    }
+
+    public static int findAmountOfElementsMoreThanAverage() {
+        int sum = 0;
+        for (int j : array1) {
+            if (j > calculateArithmeticAverage()) {
+                sum++;
+            }
+        }
+        return sum;
+    }
 }
---
+++ src/main/java/TaskForLoops.java
@@ -0,0 +1,75 @@
+
+
+public class TaskForLoops {
+    public static void main(String[] args) {
+        //firstTask();
+        //secondTask();
+        //thirdTask();
+        //fourthTask();
+        //fifthTask();
+    }
+
+    public static void firstTask() {
+
+        int coreNumber = 35;
+        int divider = 15;
+        System.out.println("Wrapped multiply digits : ");
+        for (int i = coreNumber * 10; i < 10000; i++) {
+            if (i % divider == 0 && ((i - i / 1000 * 1000) / 10) == coreNumber) {
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static void secondTask() {
+        int rightNumber = 874;
+        System.out.println(92 + "abc" + " = " + rightNumber + "*x");
+        System.out.println("x is :");
+        for (int i = 92000; i < 92999; i++) {
+            if (i % rightNumber != 0) {
+                System.out.println("[" + (i % rightNumber) + "]");
+            }
+        }
+    }
+
+    public static void thirdTask() {
+        int limit = 200;
+        int divider = 49;
+        for (int a = 0; a < limit; a++) {
+            for (int b = 0; b < limit; b++) {
+                if ((a * a + b * b) % divider == 0) {
+                    System.out.println("a=" + a + " b=" + b);
+                }
+            }
+        }
+    }
+
+    public static void fourthTask() {
+        int max = 0;
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                for (int k = 0; k < 10; k++) {
+                    int sumOfCubes = i * 100 + j * 10 + k - (i * i * i + j * j * j + k * k * k);
+                    if (sumOfCubes > max) {
+                        max = sumOfCubes;
+                    }
+                }
+            }
+        }
+        System.out.println(max);
+    }
+
+    public static void fifthTask() {
+        int count = 0;
+        for (int a = 1; a < 10; a++) {
+            for (int b = 0; b < 10; b++) {
+                for (int c = 1; c < 10; c++) {
+                    if (c != a) {
+                        count++;
+                    }
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}
